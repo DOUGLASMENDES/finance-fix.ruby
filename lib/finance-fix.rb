13809ab095a4f7360cf87ab560951dd0252f00bb
blair christensen <blair.christensen@gmail.com>
2010-11-13 01:34:04 -0600

    Add the initial FIX message parsing routine

--- lib/finance-fix.rb
+++ lib/finance-fix.rb
@@ -1,5 +1,17 @@
 module Finance # :nodoc
+  # Finance::FIX - Parse FIX protocol messages
   class FIX
     VERSION = '0.0.1'
+
+    def new
+      yield self if block_given?
+    end
+
+    # Parse "message", returning array of FIX message nodes, broken down in arrays of keys and
+    # values.
+    def parse(msg)
+      msg.split(/\x01/).collect { |pair| pair.split(/=/) }  # Split on "SOH then "="
+    end
+
   end # class FIX
 end # module Finance
--- test/test_finance-fix.rb
+++ test/test_finance-fix.rb
@@ -5,4 +5,21 @@ class TestFinanceFix < Test::Unit::TestCase
     assert          Finance::FIX.new
     assert_kind_of  Finance::FIX, Finance::FIX.new
   end
+
+  should "be able to parse a sample FIX message" do
+    # From http://en.wikipedia.org/wiki/Financial_Information_eXchange
+    msg = "8=FIX.4.29=17835=849=PHLX56=PERS52=20071123-05:30:00.00011=ATOMNOCCC999090020=3150=E39=E55=MSFT167=CS54=138=1540=244=1558=PHLX EQUITY TESTING59=047=C32=031=0151=1514=06=010=128";
+
+    Finance::FIX.new do |fix|
+      nodes = fix.parse(msg)
+    
+      assert_not_nil  nodes,          'parse() returned something'
+      assert_equal    25, nodes.size, 'parse() returned right number of nodes'
+      assert_equal    '8',        nodes[0][0],  "[0][0]   == '8'"
+      assert_equal    'FIX.4.2',  nodes[0][1],  "[0][1]   == 'FIX.4.2'"
+      assert_equal    '10',       nodes[24][0], "[24][0]  == '10'"
+      assert_equal    '128',      nodes[24][1], "[24][1]  == '128'"
+    end
+  end
+
 end
